6da63ae332d9a77729dbaaa53fef8c319807cf38
Sven Ginka <s.ginka@sensry.de>
2024-12-12 13:46:13 +0100

    spi: add pinctrl

--- drivers/spi/spi_sy1xx.c
+++ drivers/spi/spi_sy1xx.c
@@ -17,7 +17,7 @@ LOG_MODULE_REGISTER(spi_sy1xx);
 #include <zephyr/sys/__assert.h>
 #include <zephyr/sys/util.h>
 #include <udma.h>
-#include <pad_ctrl.h>
+#include <zephyr/drivers/pinctrl.h>
 
 struct sy1xx_spi_config {
 	uint32_t base;
@@ -27,16 +27,21 @@ struct sy1xx_spi_config {
 	uint8_t cpha;
 	uint8_t div;
 	uint8_t quad_spi;
+
+	const struct pinctrl_dev_config *pcfg;
 };
 
-#define DEVICE_MAX_BUFFER_SIZE (512)
+#define SY1XX_SPI_MAX_BIT_COUNT   (2048)
+#define SY1XX_SPI_MAX_BUFFER_SIZE (SY1XX_SPI_MAX_BIT_COUNT / 8)
 
 struct sy1xx_spi_data {
 	struct spi_context ctx;
 	struct k_spinlock lock;
 
-	uint8_t write[DEVICE_MAX_BUFFER_SIZE];
-	uint8_t read[DEVICE_MAX_BUFFER_SIZE];
+	uint32_t cs;
+
+	uint8_t write[SY1XX_SPI_MAX_BUFFER_SIZE];
+	uint8_t read[SY1XX_SPI_MAX_BUFFER_SIZE];
 };
 
 struct sy1xx_spi_xfer {
@@ -97,6 +102,13 @@ struct sy1xx_spi_xfer {
 #define SPI_CMD_READ_DATA2(num_bits) (UINT16_BYTE0((num_bits - 1)) & 0xffff)
 #define SPI_CMD_READ_DATA3(num_bits) (UINT16_BYTE1((num_bits - 1)) & 0xffff)
 
+/* CMD READ_DATA */
+#define SPI_CMD_FULL_DPLX_DATA0(qspi, align)                                                       \
+	(SPI_CMD_FULL_DPLX | ((qspi & 0x1) << 3) | ((align & 0x3) << 1))
+#define SPI_CMD_FULL_DPLX_DATA1()         (0)
+#define SPI_CMD_FULL_DPLX_DATA2(num_bits) (UINT16_BYTE0((num_bits - 1)) & 0xffff)
+#define SPI_CMD_FULL_DPLX_DATA3(num_bits) (UINT16_BYTE1((num_bits - 1)) & 0xffff)
+
 /* CMD EOT */
 #define SPI_CMD_EOT0()    (SPI_CMD_EOT)
 #define SPI_CMD_EOT1()    (0)
@@ -130,7 +142,7 @@ static int sy1xx_spi_init(const struct device *dev)
 	struct sy1xx_spi_config *config = (struct sy1xx_spi_config *)dev->config;
 	struct sy1xx_spi_data *data = (struct sy1xx_spi_data *)dev->data;
 
-	for (uint32_t i = 0; i < DEVICE_MAX_BUFFER_SIZE; i++) {
+	for (uint32_t i = 0; i < SY1XX_SPI_MAX_BUFFER_SIZE; i++) {
 		data->write[i] = 0x22;
 		data->read[i] = 0x44;
 	}
@@ -139,35 +151,11 @@ static int sy1xx_spi_init(const struct device *dev)
 	sy1xx_udma_enable_clock(SY1XX_UDMA_MODULE_SPI, config->inst);
 
 	/* PAD config */
-	uint32_t pad_config_cs1 =
-		SY1XX_PAD_CONFIG(0, SY1XX_PAD_SMT_DISABLE, SY1XX_PAD_SLEW_LOW, SY1XX_PAD_PULLUP_EN,
-				 SY1XX_PAD_PULLDOWN_DIS, SY1XX_PAD_DRIVE_2PF, SY1XX_PAD_PMOD_NORMAL,
-				 SY1XX_PAD_DIR_OUTPUT);
+	int32_t ret = pinctrl_apply_state(config->pcfg, PINCTRL_STATE_DEFAULT);
 
-	uint32_t pad_config_cs0 =
-		SY1XX_PAD_CONFIG(8, SY1XX_PAD_SMT_DISABLE, SY1XX_PAD_SLEW_LOW, SY1XX_PAD_PULLUP_EN,
-				 SY1XX_PAD_PULLDOWN_DIS, SY1XX_PAD_DRIVE_2PF, SY1XX_PAD_PMOD_NORMAL,
-				 SY1XX_PAD_DIR_OUTPUT);
-
-	uint32_t pad_config_miso =
-		SY1XX_PAD_CONFIG(16, SY1XX_PAD_SMT_DISABLE, SY1XX_PAD_SLEW_LOW,
-				 SY1XX_PAD_PULLUP_DIS, SY1XX_PAD_PULLDOWN_DIS, SY1XX_PAD_DRIVE_2PF,
-				 SY1XX_PAD_PMOD_NORMAL, SY1XX_PAD_DIR_INPUT);
-
-	uint32_t pad_config_mosi =
-		SY1XX_PAD_CONFIG(24, SY1XX_PAD_SMT_DISABLE, SY1XX_PAD_SLEW_LOW,
-				 SY1XX_PAD_PULLUP_DIS, SY1XX_PAD_PULLDOWN_DIS, SY1XX_PAD_DRIVE_2PF,
-				 SY1XX_PAD_PMOD_NORMAL, SY1XX_PAD_DIR_OUTPUT);
-
-	uint32_t pad_config_clk =
-		SY1XX_PAD_CONFIG(0, SY1XX_PAD_SMT_DISABLE, SY1XX_PAD_SLEW_LOW, SY1XX_PAD_PULLUP_DIS,
-				 SY1XX_PAD_PULLDOWN_DIS, SY1XX_PAD_DRIVE_2PF, SY1XX_PAD_PMOD_NORMAL,
-				 SY1XX_PAD_DIR_OUTPUT);
-
-	/* write 2 registers for pad config per instance */
-	sys_write32((pad_config_cs1 | pad_config_cs0 | pad_config_miso | pad_config_mosi),
-		    SY1XX_PAD_CONFIG_ADDR_SPI + (config->inst * 8 + 0));
-	sys_write32((pad_config_clk), SY1XX_PAD_CONFIG_ADDR_SPI + (config->inst * 8 + 4));
+	if (ret < 0) {
+		LOG_ERR("SPI failed to set pin config for %u", config->inst);
+	}
 
 	config->cpol = 0;
 	config->cpha = 0;
@@ -190,138 +178,119 @@ void print_hex(uint8_t *buf, uint32_t len)
 	printf("\n");
 }
 
-/**
- *
- * with the given device config and requested spi transfer, prepare
- * the header of the transfer buffer
- * @return:
- *  - < 0: Error
- *  - = 0: Nothing to do
- *  - > 0: Number of data bytes to transfer
- */
-static int32_t sy1xx_spi_prepare_header(const struct device *dev, uint8_t *buf, uint32_t *count,
-					struct sy1xx_spi_xfer *request)
+static int32_t sy1xx_spi_full_duplex_transfer(const struct device *dev, uint8_t *tx_buf,
+					      uint8_t *rx_buf, uint32_t xfer_len)
 {
 	struct sy1xx_spi_config *config = (struct sy1xx_spi_config *)dev->config;
+	struct sy1xx_spi_data *data = (struct sy1xx_spi_data *)dev->data;
 
-	if (request->data_len == 0) {
+	printf("SPI transfer %d bytes\n", xfer_len);
+
+	if (xfer_len == 0) {
 		return -EINVAL;
 	}
 
-	if (request->data_len > DEVICE_MAX_BUFFER_SIZE) {
-		return -EINVAL;
+	uint8_t *cmd_buf = &data->write[0];
+	uint32_t count = 0;
+
+	/* expected data config (bitlen in bits) and spi transfer type */
+	cmd_buf[count++] = SPI_CMD_FULL_DPLX_DATA3(xfer_len * 8);
+	cmd_buf[count++] = SPI_CMD_FULL_DPLX_DATA2(xfer_len * 8);
+	cmd_buf[count++] = SPI_CMD_FULL_DPLX_DATA1();
+	cmd_buf[count++] = SPI_CMD_FULL_DPLX_DATA0(config->quad_spi, 0);
+
+	/* data; we need to fill multiple of 32bit size */
+	uint32_t padded_len = ROUND_UP_DIV_4(xfer_len) * 4;
+
+	for (uint32_t i = 0; i < padded_len; i++) {
+		cmd_buf[count + i] = (i < xfer_len) ? tx_buf[i] : 0xff;
 	}
 
-	/* stop any prior transmission */
+	count += padded_len;
+
+	SY1XX_UDMA_START_RX(config->base, (uint32_t)data->read, xfer_len,
+			    SY1XX_UDMA_RX_DATA_ADDR_INC_SIZE_32);
+
+	SY1XX_UDMA_START_TX(config->base, (uint32_t)cmd_buf, count, 0);
+
 	SY1XX_UDMA_WAIT_FOR_FINISHED_TX(config->base);
 	SY1XX_UDMA_WAIT_FOR_FINISHED_RX(config->base);
 
-	if (request->data_len > 0) {
-		/* spi cfg */
-		buf[(*count)++] = SPI_CMD_CFG_3(config->div);
-		buf[(*count)++] = SPI_CMD_CFG_2(config->cpol, config->cpha);
-		buf[(*count)++] = SPI_CMD_CFG_1();
-		buf[(*count)++] = SPI_CMD_CFG_0();
+	int32_t transferred_count = (int32_t)xfer_len - SY1XX_UDMA_GET_REMAINING_RX(config->base);
 
-		/* start with selecting chip-select */
-		buf[(*count)++] = SPI_CMD_SOT_3(request->cs);
-		buf[(*count)++] = SPI_CMD_SOT_2();
-		buf[(*count)++] = SPI_CMD_SOT_1();
-		buf[(*count)++] = SPI_CMD_SOT_0();
-
-		if (request->addr_width > 0) {
-			/* set address if configured */
-			buf[(*count)++] = SPI_CMD_SEND_ADDR3();
-			buf[(*count)++] = SPI_CMD_SEND_ADDR2();
-			buf[(*count)++] = SPI_CMD_SEND_ADDR1(request->addr_width);
-			buf[(*count)++] = SPI_CMD_SEND_ADDR0(config->quad_spi);
-
-			/* based on width, shift */
-			uint32_t addr;
-
-			if (request->addr_width <= 8) {
-				addr = request->addr << 24;
-			} else if (request->addr_width <= 16) {
-				addr = request->addr << 16;
-			} else if (request->addr_width <= 24) {
-				addr = request->addr << 8;
-			} else {
-				addr = request->addr;
-			}
-
-			/* addr is always uint32 */
-			buf[(*count)++] = UINT32_BYTE3(addr);
-			buf[(*count)++] = UINT32_BYTE2(addr);
-			buf[(*count)++] = UINT32_BYTE1(addr);
-			buf[(*count)++] = UINT32_BYTE0(addr);
-		}
-
-		return request->data_len;
+	if ((transferred_count > 0) && (transferred_count <= xfer_len)) {
+		memcpy(rx_buf, &data->read[0], transferred_count);
+		printf("TX>");
+		print_hex(cmd_buf, count);
+		printf("RX>");
+		print_hex(rx_buf, transferred_count);
+		return transferred_count;
 	}
 
 	return 0;
 }
 
-/**
- * @return:
- *  - < 0: Error
- *  - = 0: No bytes written
- *  - > 0: Number of bytes written
- */
-static int32_t sy1xx_spi_write(const struct device *dev, struct sy1xx_spi_xfer *request)
+static int32_t sy1xx_spi_set_cs(const struct device *dev, uint32_t cs)
 {
-
 	struct sy1xx_spi_config *config = (struct sy1xx_spi_config *)dev->config;
 	struct sy1xx_spi_data *data = (struct sy1xx_spi_data *)dev->data;
 
-	uint8_t *buf = &data->write[0];
+	printf("SPI set CS%d\n", cs);
 
+	uint8_t *cmd_buf = &data->write[0];
 	uint32_t count = 0;
 
-	/* prepare start of transfer and address */
-	int32_t ret = sy1xx_spi_prepare_header(dev, buf, &count, request);
+	/* stop any prior transmission */
+	SY1XX_UDMA_WAIT_FOR_FINISHED_TX(config->base);
+	SY1XX_UDMA_WAIT_FOR_FINISHED_RX(config->base);
 
-	if (ret < 0) {
-		LOG_ERR("Failed to prepare transfer");
-		return -EINVAL;
-	}
+	/* spi cfg */
+	cmd_buf[count++] = SPI_CMD_CFG_3(config->div);
+	cmd_buf[count++] = SPI_CMD_CFG_2(config->cpol, config->cpha);
+	cmd_buf[count++] = SPI_CMD_CFG_1();
+	cmd_buf[count++] = SPI_CMD_CFG_0();
 
-	if (ret == 0) {
-		LOG_WRN("Nothing to do");
-		return 0;
-	}
+	/* start with selecting chip-select */
+	cmd_buf[count++] = SPI_CMD_SOT_3(cs);
+	cmd_buf[count++] = SPI_CMD_SOT_2();
+	cmd_buf[count++] = SPI_CMD_SOT_1();
+	cmd_buf[count++] = SPI_CMD_SOT_0();
 
-	/* expected data config and spi transfer type */
-	buf[count++] = SPI_CMD_SEND_DATA3(request->data_len * 8);
-	buf[count++] = SPI_CMD_SEND_DATA2(request->data_len * 8);
-	buf[count++] = SPI_CMD_SEND_DATA1();
-	buf[count++] = SPI_CMD_SEND_DATA0(config->quad_spi);
-
-	/* data; we need to fill multiple of 32bit size */
-	uint32_t word_count = ROUND_UP_DIV_4(request->data_len);
-
-	for (uint32_t i = 0; i < request->data_len; i++) {
-		buf[count + i] = request->data[i];
-	}
-	count += (word_count * 4);
-
-	/* end of transmission */
-	buf[count++] = SPI_CMD_EOT3(0);
-	buf[count++] = SPI_CMD_EOT2();
-	buf[count++] = SPI_CMD_EOT1();
-	buf[count++] = SPI_CMD_EOT0();
-
-	SY1XX_UDMA_START_TX(config->base, (uint32_t)buf, count, 0);
+	SY1XX_UDMA_START_TX(config->base, (uint32_t)cmd_buf, count, 0);
 
 	SY1XX_UDMA_WAIT_FOR_FINISHED_TX(config->base);
 
-	int32_t xfer_count = request->data_len - SY1XX_UDMA_GET_REMAINING_RX(config->base);
-
-	if ((xfer_count > 0) && (xfer_count <= request->data_len)) {
-		print_hex(buf, count);
-		return xfer_count;
+	int32_t xfer_count = (int32_t)count - SY1XX_UDMA_GET_REMAINING_TX(config->base);
+	if (xfer_count != count) {
+		return -EINVAL;
 	}
+	return 0;
+}
 
+static int32_t sy1xx_spi_reset_cs(const struct device *dev)
+{
+	struct sy1xx_spi_config *config = (struct sy1xx_spi_config *)dev->config;
+	struct sy1xx_spi_data *data = (struct sy1xx_spi_data *)dev->data;
+
+	printf("SPI reset CS\n");
+
+	uint8_t *cmd_buf = &data->write[0];
+	uint32_t count = 0;
+
+	/* end of transmission */
+	cmd_buf[count++] = SPI_CMD_EOT3(0);
+	cmd_buf[count++] = SPI_CMD_EOT2();
+	cmd_buf[count++] = SPI_CMD_EOT1();
+	cmd_buf[count++] = SPI_CMD_EOT0();
+
+	SY1XX_UDMA_START_TX(config->base, (uint32_t)cmd_buf, count, 0);
+
+	SY1XX_UDMA_WAIT_FOR_FINISHED_TX(config->base);
+
+	int32_t xfer_count = (int32_t)count - SY1XX_UDMA_GET_REMAINING_TX(config->base);
+	if (xfer_count != count) {
+		return -EINVAL;
+	}
 	return 0;
 }
 
@@ -331,56 +300,90 @@ static int32_t sy1xx_spi_write(const struct device *dev, struct sy1xx_spi_xfer *
  *  - = 0: No bytes received
  *  - > 0: Number of bytes received
  */
-static int32_t sy1xx_spi_read(const struct device *dev, struct sy1xx_spi_xfer *request)
+static int32_t sy1xx_spi_half_duplex_read(const struct device *dev, uint8_t *rx_buf,
+					  uint32_t rx_len)
 {
 
 	struct sy1xx_spi_config *config = (struct sy1xx_spi_config *)dev->config;
 	struct sy1xx_spi_data *data = (struct sy1xx_spi_data *)dev->data;
 
-	uint8_t *buf = &data->write[0];
+	printf("SPI read %d bytes\n", rx_len);
 
+	uint8_t *cmd_buf = &data->write[0];
 	uint32_t count = 0;
 
-	/* prepare start of transfer and address */
-	int32_t ret = sy1xx_spi_prepare_header(dev, buf, &count, request);
-
-	if (ret < 0) {
-		LOG_ERR("Failed to prepare transfer");
-		return -EINVAL;
-	}
-
-	if (ret == 0) {
-		LOG_WRN("Nothing to do");
-		return 0;
-	}
-
 	/* expected data config */
-	buf[count++] = SPI_CMD_READ_DATA3(request->data_len * 8);
-	buf[count++] = SPI_CMD_READ_DATA2(request->data_len * 8);
-	buf[count++] = SPI_CMD_READ_DATA1();
-	buf[count++] = SPI_CMD_READ_DATA0(0, 0);
+	cmd_buf[count++] = SPI_CMD_READ_DATA3(rx_len * 8);
+	cmd_buf[count++] = SPI_CMD_READ_DATA2(rx_len * 8);
+	cmd_buf[count++] = SPI_CMD_READ_DATA1();
+	cmd_buf[count++] = SPI_CMD_READ_DATA0(0, 0);
 
-	/* end of transmission */
-	buf[count++] = SPI_CMD_EOT3(0);
-	buf[count++] = SPI_CMD_EOT2();
-	buf[count++] = SPI_CMD_EOT1();
-	buf[count++] = SPI_CMD_EOT0();
-
-	SY1XX_UDMA_START_RX(config->base, (uint32_t)data->read, request->data_len,
+	SY1XX_UDMA_START_RX(config->base, (uint32_t)data->read, rx_len,
 			    SY1XX_UDMA_RX_DATA_ADDR_INC_SIZE_32);
-	SY1XX_UDMA_START_TX(config->base, (uint32_t)buf, count, 0);
+	SY1XX_UDMA_START_TX(config->base, (uint32_t)cmd_buf, count, 0);
 
 	SY1XX_UDMA_WAIT_FOR_FINISHED_TX(config->base);
 	SY1XX_UDMA_WAIT_FOR_FINISHED_RX(config->base);
 
-	int32_t xfer_count = request->data_len - SY1XX_UDMA_GET_REMAINING_RX(config->base);
+	int32_t xfer_count = (int32_t)rx_len - SY1XX_UDMA_GET_REMAINING_RX(config->base);
 
-	if ((xfer_count > 0) && (xfer_count <= request->data_len)) {
-		memcpy(request->data, &data->read[0], xfer_count);
+	if ((xfer_count > 0) && (xfer_count <= rx_len)) {
+		memcpy(rx_buf, &data->read[0], xfer_count);
+		printf("RX>");
+		print_hex(rx_buf, xfer_count);
+
 		return xfer_count;
 	}
 
-	return 0;
+	return -EINVAL;
+}
+
+/**
+ * @return:
+ *  - < 0: Error
+ *  - = 0: No bytes written
+ *  - > 0: Number of bytes written
+ */
+static int32_t sy1xx_spi_half_duplex_write(const struct device *dev, uint8_t *tx_buf,
+					   uint32_t tx_len)
+{
+
+	struct sy1xx_spi_config *config = (struct sy1xx_spi_config *)dev->config;
+	struct sy1xx_spi_data *data = (struct sy1xx_spi_data *)dev->data;
+
+	printf("SPI write %d bytes\n", tx_len);
+
+	uint8_t *cmd_buf = &data->write[0];
+	uint32_t count = 0;
+
+	/* expected data config and spi transfer type */
+	cmd_buf[count++] = SPI_CMD_SEND_DATA3(tx_len * 8);
+	cmd_buf[count++] = SPI_CMD_SEND_DATA2(tx_len * 8);
+	cmd_buf[count++] = SPI_CMD_SEND_DATA1();
+	cmd_buf[count++] = SPI_CMD_SEND_DATA0(config->quad_spi);
+
+	/* data; we need to fill multiple of 32bit size */
+	uint32_t padded_len = ROUND_UP_DIV_4(tx_len) * 4;
+
+	for (uint32_t i = 0; i < padded_len; i++) {
+		cmd_buf[count + i] = (i < tx_len) ? tx_buf[i] : 0xff;
+	}
+
+	count += padded_len;
+
+	SY1XX_UDMA_START_TX(config->base, (uint32_t)cmd_buf, count, 0);
+
+	SY1XX_UDMA_WAIT_FOR_FINISHED_TX(config->base);
+
+	int32_t xfer_count = (int32_t)tx_len - SY1XX_UDMA_GET_REMAINING_TX(config->base);
+
+	if ((xfer_count > 0) && (xfer_count <= tx_len)) {
+		printf("TX>");
+		print_hex(cmd_buf, count);
+		return xfer_count;
+	}
+
+	return -EINVAL;
 }
 
 /**
@@ -405,86 +408,78 @@ static int sy1xx_spi_transceive_sync(const struct device *dev, const struct spi_
 				     const struct spi_buf_set *tx_bufs,
 				     const struct spi_buf_set *rx_bufs)
 {
+	struct sy1xx_spi_data *data = (struct sy1xx_spi_data *)dev->data;
+
 	int ret = -EINVAL;
+	size_t tx_count = 0;
+	size_t rx_count = 0;
+	const struct spi_buf *tx = NULL;
+	const struct spi_buf *rx = NULL;
 
-	uint32_t tx_buf_count = 0;
-	uint32_t rx_buf_count = 0;
-
-	if (tx_bufs != NULL) {
-		tx_buf_count = tx_bufs->count;
+	if (tx_bufs) {
+		tx = tx_bufs->buffers;
+		tx_count = tx_bufs->count;
 	}
 
-	if (rx_bufs != NULL) {
-		rx_buf_count = rx_bufs->count;
+	if (rx_bufs) {
+		rx = rx_bufs->buffers;
+		rx_count = rx_bufs->count;
 	}
 
-	struct sy1xx_spi_xfer request = {
-		.cs = 0,
-		.addr_width = 0,
-		.addr = 0,
+	volatile uint32_t x = FIELD_GET(0x001f0000, 0x11f23344);
+	printf("x = %x\n", x);
 
-		.data = 0,
-		.data_len = 0,
-	};
+	data->cs = 0;
+	sy1xx_spi_set_cs(dev, data->cs);
 
-	if ((tx_buf_count == 0) && (rx_buf_count == 1)) {
-		/* pure read */
+	/* handle symmetrical tx and rx transfers */
+	while (tx_count != 0 && rx_count != 0) {
+		if (tx->buf == NULL) {
+			ret = sy1xx_spi_half_duplex_read(dev, rx->buf, rx->len);
+		} else if (rx->buf == NULL) {
+			ret = sy1xx_spi_half_duplex_write(dev, tx->buf, tx->len);
+		} else if (rx->len == tx->len) {
+			ret = sy1xx_spi_full_duplex_transfer(dev, tx->buf, rx->buf, rx->len);
+		} else {
+			__ASSERT_NO_MSG("Invalid SPI transfer configuration");
+		}
 
-		request.data_len = rx_bufs->buffers[0].len;
-		request.data = rx_bufs->buffers[0].buf;
+		if (ret < 0) {
+			sy1xx_spi_reset_cs(dev);
+			LOG_ERR("Failed to transfer data");
+			return ret;
+		}
 
-		ret = sy1xx_spi_read(dev, &request);
-
-	} else if ((tx_buf_count == 1) && (rx_buf_count == 0)) {
-		/* pure write */
-
-		request.data_len = tx_bufs->buffers[0].len;
-		request.data = tx_bufs->buffers[0].buf;
-
-		ret = sy1xx_spi_write(dev, &request);
-
-	} else if ((tx_buf_count == 1) && (rx_buf_count == 2)) {
-		/* read from address */
-
-		/* decode address */
-		request.addr_width = 8;
-		request.addr = ((uint8_t *)tx_bufs->buffers[0].buf)[0];
-
-		/* decode data */
-		request.data = (uint8_t *)rx_bufs->buffers[1].buf;
-		request.data_len = rx_bufs->buffers[1].len;
-
-		ret = sy1xx_spi_read(dev, &request);
-
-	} else if ((tx_buf_count == 2) && (rx_buf_count == 0)) {
-		/* write to address */
-
-		/* decode address */
-		request.addr_width = 8;
-		request.addr = ((uint8_t *)tx_bufs->buffers[0].buf)[0];
-
-		/* decode data */
-		request.data = (uint8_t *)tx_bufs->buffers[1].buf;
-		request.data_len = tx_bufs->buffers[1].len;
-
-		ret = sy1xx_spi_write(dev, &request);
-	} else {
-		LOG_ERR("Unsupported spi transfer mode");
-		return -EINVAL;
+		tx++;
+		tx_count--;
+		rx++;
+		rx_count--;
 	}
 
-	if (ret == request.data_len) {
-		/* success */
-		return 0;
+	/* handle the left-overs for tx only */
+	for (; tx_count != 0; tx_count--) {
+		ret = sy1xx_spi_half_duplex_write(dev, tx->buf, tx->len);
+		if (ret < 0) {
+			sy1xx_spi_reset_cs(dev);
+			LOG_ERR("Failed to transfer data");
+			return ret;
+		}
+		tx++;
 	}
 
-	if (ret < 0) {
-		LOG_ERR("Failed spi transfer: %d", ret);
-		return ret;
+	/* handle the left-overs for rx only */
+	for (; rx_count != 0; rx_count--) {
+		ret = sy1xx_spi_half_duplex_read(dev, rx->buf, rx->len);
+		if (ret < 0) {
+			sy1xx_spi_reset_cs(dev);
+			LOG_ERR("Failed to transfer data");
+			return ret;
+		}
+		rx++;
 	}
 
-	LOG_WRN("Invalid number of data transferred");
-	return -EPIPE;
+	sy1xx_spi_reset_cs(dev);
+	return 0;
 }
 
 static int sy1xx_spi_release(const struct device *dev, const struct spi_config *config)
@@ -500,9 +495,12 @@ static const struct spi_driver_api sy1xx_spi_driver_api = {
 
 #define SPI_SY1XX_DEVICE_INIT(n)                                                                   \
                                                                                                    \
+	PINCTRL_DT_INST_DEFINE(n);                                                                 \
+                                                                                                   \
 	static struct sy1xx_spi_config sy1xx_spi_dev_config_##n = {                                \
 		.base = (uint32_t)DT_INST_REG_ADDR(n),                                             \
 		.inst = (uint32_t)DT_INST_PROP(n, instance),                                       \
+		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),                                         \
 	};                                                                                         \
                                                                                                    \
 	static struct sy1xx_spi_data __attribute__((section(".udma_access")))                      \
