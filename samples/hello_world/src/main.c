fb0bfadf93adb9c8eab9f470c5898b5ec3162e90
Sven Ginka <s.ginka@sensry.de>
2026-06-30 10:57:27 +0200

    mod: hello_world test

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -4,11 +4,27 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdint.h>
 #include <stdio.h>
+
+static uint32_t data_buffer[10] = {
+	0x1234,
+	0x5678,
+	0xaaaa,
+	0xbbbb,
+	0xcccc,
+	0xdddd,
+};
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	printf("%p\n", data_buffer);
+
+	for (uint32_t i = 0; i < 10; i++) {
+		printf("data_buffer[%d] = 0x%x\n", i, data_buffer[i]);
+	}
 
 	return 0;
 }
