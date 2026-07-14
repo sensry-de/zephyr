cfc433b07c4ab6588672749f70167fa24a3d0e62
Sven Ginka <s.ginka@sensry.de>
2026-07-14 10:29:19 +0200

    mod: enable support for sam-ba bootloader (via bossac)

--- boards/heimann/htpa_eval/board.cmake
+++ boards/heimann/htpa_eval/board.cmake
@@ -2,3 +2,6 @@
 
 board_runner_args(openocd --cmd-post-verify "atsamv gpnvm set 1")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+
+board_runner_args(bossac "--bossac-port=/dev/ttyACM0")
+include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
