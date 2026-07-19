b03f6f21fbe9b62b6aa7f37fe8fe08b54c3ba67c
Sven Ginka <s.ginka@sensry.de>
2026-07-19 12:51:00 +0200

    wip

--- drivers/video/video_hm_htpa/htpa-common.c
+++ drivers/video/video_hm_htpa/htpa-common.c
@@ -220,7 +220,7 @@ static int hm_htpa_flush(const struct device *dev, bool cancel)
 	return 0;
 }
 
-DEVICE_API(video, hm_htpa_api) = {
+static DEVICE_API(video, hm_htpa_api) = {
 	.set_format = hm_htpa_set_fmt,
 	.get_format = hm_htpa_get_fmt,
 	.set_stream = hm_htpa_set_stream,
@@ -230,7 +230,7 @@ DEVICE_API(video, hm_htpa_api) = {
 	.flush = hm_htpa_flush,
 };
 
-int hm_htpa_init(const struct device *dev)
+static int hm_htpa_init(const struct device *dev)
 {
 	const struct hm_htpa_config *cfg = dev->config;
 	struct hm_htpa_data *data = dev->data;
@@ -301,3 +301,148 @@ int hm_htpa_init(const struct device *dev)
 
 	return 0;
 }
+
+#define HTPA_DEFAULT_ACQUISITION_TIME_USEC 1000
+
+#define HM_HTPA_DEFINE(inst, model, width, height, block_count, block_length, caps, sensor_cfg)    \
+	static int16_t __aligned(4) hm_htpa_pixels_##model##_##inst[HTPA_FRAME_QUEUE_SIZE]         \
+								   [(width) * (height)];           \
+	static uint8_t __aligned(4) hm_htpa_raw_top_##model##_##inst[block_count][block_length];   \
+	static uint8_t                                                                             \
+		__aligned(4) hm_htpa_raw_bottom_##model##_##inst[block_count][block_length];       \
+	static uint8_t __aligned(4) hm_htpa_el_top_##model##_##inst[block_length];                 \
+	static uint8_t __aligned(4) hm_htpa_el_bottom_##model##_##inst[block_length];              \
+	static const struct hm_htpa_config hm_htpa_cfg_##model##_##inst = {                        \
+		.hm_htpa_caps = caps,                                                              \
+		.calibration_flash = DEVICE_DT_GET(DT_INST_PHANDLE(inst, calibration_flash)),      \
+		.sensor = sensor_cfg,                                                              \
+	};                                                                                         \
+	static struct hm_htpa_data hm_htpa_data_##model##_##inst = {                               \
+		.grab =                                                                            \
+			{                                                                          \
+				.acquisition_time = HTPA_DEFAULT_ACQUISITION_TIME_USEC,            \
+				.frames = {{.pixels = hm_htpa_pixels_##model##_##inst[0]},         \
+					   {.pixels = hm_htpa_pixels_##model##_##inst[1]}},        \
+				.raw_top = &hm_htpa_raw_top_##model##_##inst[0][0],                \
+				.raw_bottom = &hm_htpa_raw_bottom_##model##_##inst[0][0],          \
+				.el_top_offsets = hm_htpa_el_top_##model##_##inst,                 \
+				.el_bottom_offsets = hm_htpa_el_bottom_##model##_##inst,           \
+			},                                                                         \
+		.spi = SPI_DT_SPEC_INST_GET(inst, SPI_OP_MODE_MASTER | SPI_WORD_SET(8) |           \
+							  SPI_LINES_SINGLE),                       \
+	};                                                                                         \
+	DEVICE_DT_INST_DEFINE(inst, hm_htpa_init, NULL, &hm_htpa_data_##model##_##inst,            \
+			      &hm_htpa_cfg_##model##_##inst, POST_KERNEL,                          \
+			      CONFIG_VIDEO_INIT_PRIORITY, &hm_htpa_api);
+
+#if defined(CONFIG_DT_HAS_HEIMANN_HTPA_120X84_ENABLED)
+
+#define DT_DRV_COMPAT heimann_htpa_120x84
+
+enum {
+	HTPA_WIDTH_120X84 = 120,
+	HTPA_HEIGHT_120X84 = 84,
+	HTPA_BLOCK_COUNT_120X84 = 6,
+	HTPA_PIXELS_PER_BLOCK_120X84 = 840,
+	HTPA_BLOCK_LENGTH_120X84 = 1682,
+	HTPA_DATA_OFFSET_120X84 = 6,
+};
+
+#if defined(CONFIG_VIDEO_HM_HTPA_AUTOSCALE_HISTOGRAM)
+BUILD_ASSERT((HTPA_WIDTH_120X84 * HTPA_HEIGHT_120X84) <= UINT16_MAX,
+	     "Histogram counter type is too small");
+#endif
+
+static const struct hm_htpa_sensor_config hm_htpa_sensor_cfg_120x84 = {
+	.width = HTPA_WIDTH_120X84,
+	.height = HTPA_HEIGHT_120X84,
+	.block_count = HTPA_BLOCK_COUNT_120X84,
+	.pixels_per_block = HTPA_PIXELS_PER_BLOCK_120X84,
+	.block_length = HTPA_BLOCK_LENGTH_120X84,
+	.data_offset = HTPA_DATA_OFFSET_120X84,
+	.e_id = {0x0074, 0x0075, 0x0076, 0x0077},
+	.e_mbit_calib = 0x001a,
+	.e_bias_calib = 0x001b,
+	.e_clk_calib = 0x001c,
+	.e_bpa_calib = 0x001d,
+};
+
+static const struct video_format_cap hm_htpa_caps_120x84[] = {
+	{
+		.pixelformat = VIDEO_PIX_FMT_Y16,
+		.width_min = HTPA_WIDTH_120X84,
+		.width_max = HTPA_WIDTH_120X84,
+		.height_min = HTPA_HEIGHT_120X84,
+		.height_max = HTPA_HEIGHT_120X84,
+		.width_step = 0,
+		.height_step = 0,
+	},
+	{0},
+};
+
+#define HM_HTPA_INIT_120X84(inst)                                                                  \
+	HM_HTPA_DEFINE(inst, htpa_120x84, HTPA_WIDTH_120X84, HTPA_HEIGHT_120X84,                   \
+		       HTPA_BLOCK_COUNT_120X84, HTPA_BLOCK_LENGTH_120X84, hm_htpa_caps_120x84,     \
+		       &hm_htpa_sensor_cfg_120x84)
+
+DT_INST_FOREACH_STATUS_OKAY(HM_HTPA_INIT_120X84)
+
+#undef DT_DRV_COMPAT
+
+#endif /* CONFIG_DT_HAS_HEIMANN_HTPA_120X84_ENABLED */
+
+#if defined(CONFIG_DT_HAS_HEIMANN_HTPA_160X120_ENABLED)
+
+#define DT_DRV_COMPAT heimann_htpa_160x120
+
+enum {
+	HTPA_WIDTH_160X120 = 160,
+	HTPA_HEIGHT_160X120 = 120,
+	HTPA_BLOCK_COUNT_160X120 = 12,
+	HTPA_PIXELS_PER_BLOCK_160X120 = 800,
+	HTPA_BLOCK_LENGTH_160X120 = 1606,
+	HTPA_DATA_OFFSET_160X120 = 6,
+};
+
+#if defined(CONFIG_VIDEO_HM_HTPA_AUTOSCALE_HISTOGRAM)
+BUILD_ASSERT((HTPA_WIDTH_160X120 * HTPA_HEIGHT_160X120) <= UINT16_MAX,
+	     "Histogram counter type is too small");
+#endif
+
+static const struct hm_htpa_sensor_config hm_htpa_sensor_cfg_160x120 = {
+	.width = HTPA_WIDTH_160X120,
+	.height = HTPA_HEIGHT_160X120,
+	.block_count = HTPA_BLOCK_COUNT_160X120,
+	.pixels_per_block = HTPA_PIXELS_PER_BLOCK_160X120,
+	.block_length = HTPA_BLOCK_LENGTH_160X120,
+	.data_offset = HTPA_DATA_OFFSET_160X120,
+	.e_id = {0x0074, 0x0075, 0x0076, 0x0077},
+	.e_mbit_calib = 0x001a,
+	.e_bias_calib = 0x001b,
+	.e_clk_calib = 0x001c,
+	.e_bpa_calib = 0x001d,
+};
+
+static const struct video_format_cap hm_htpa_caps_160x120[] = {
+	{
+		.pixelformat = VIDEO_PIX_FMT_Y16,
+		.width_min = HTPA_WIDTH_160X120,
+		.width_max = HTPA_WIDTH_160X120,
+		.height_min = HTPA_HEIGHT_160X120,
+		.height_max = HTPA_HEIGHT_160X120,
+		.width_step = 0,
+		.height_step = 0,
+	},
+	{0},
+};
+
+#define HM_HTPA_INIT_160X120(inst)                                                                 \
+	HM_HTPA_DEFINE(inst, htpa_160x120, HTPA_WIDTH_160X120, HTPA_HEIGHT_160X120,                \
+		       HTPA_BLOCK_COUNT_160X120, HTPA_BLOCK_LENGTH_160X120, hm_htpa_caps_160x120,  \
+		       &hm_htpa_sensor_cfg_160x120)
+
+DT_INST_FOREACH_STATUS_OKAY(HM_HTPA_INIT_160X120)
+
+#undef DT_DRV_COMPAT
+
+#endif /* CONFIG_DT_HAS_HEIMANN_HTPA_160X120_ENABLED */
