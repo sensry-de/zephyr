02bf3b3d36cc4dc132ff76288dd787ab789e0e9e
Sven Ginka <s.ginka@sensry.de>
2026-07-19 10:34:25 +0200

    wip

--- drivers/video/video_hm_htpa/htpa-120x84.c
+++ drivers/video/video_hm_htpa/htpa-120x84.c
@@ -13,17 +13,27 @@ LOG_MODULE_REGISTER(htpa_120x84, CONFIG_VIDEO_LOG_LEVEL);
 
 #define HTPA_DEFAULT_ACQUISITION_TIME_USEC 1000
 
+enum {
+	HTPA_WIDTH_120X84 = 120,
+	HTPA_HEIGHT_120X84 = 84,
+	HTPA_BLOCK_COUNT_120X84 = 6,
+	HTPA_PIXELS_PER_BLOCK_120X84 = 840,
+	HTPA_BLOCK_LENGTH_120X84 = 1682,
+	HTPA_DATA_OFFSET_120X84 = 6,
+};
+
 #if defined(CONFIG_VIDEO_HM_HTPA_AUTOSCALE_HISTOGRAM)
-BUILD_ASSERT((120 * 84) <= UINT16_MAX, "Histogram counter type is too small");
+BUILD_ASSERT((HTPA_WIDTH_120X84 * HTPA_HEIGHT_120X84) <= UINT16_MAX,
+	     "Histogram counter type is too small");
 #endif
 
 static const struct hm_htpa_sensor_config hm_htpa_sensor_cfg_120x84 = {
-	.width = 120,
-	.height = 84,
-	.block_count = 6,
-	.pixels_per_block = 840,
-	.block_length = 1682,
-	.data_offset = 6,
+	.width = HTPA_WIDTH_120X84,
+	.height = HTPA_HEIGHT_120X84,
+	.block_count = HTPA_BLOCK_COUNT_120X84,
+	.pixels_per_block = HTPA_PIXELS_PER_BLOCK_120X84,
+	.block_length = HTPA_BLOCK_LENGTH_120X84,
+	.data_offset = HTPA_DATA_OFFSET_120X84,
 	.e_id = {0x0074, 0x0075, 0x0076, 0x0077},
 	.e_mbit_calib = 0x001a,
 	.e_bias_calib = 0x001b,
@@ -33,22 +43,27 @@ static const struct hm_htpa_sensor_config hm_htpa_sensor_cfg_120x84 = {
 
 static const struct video_format_cap hm_htpa_caps_120x84[] = {{
 							       .pixelformat = VIDEO_PIX_FMT_Y16,
-							       .width_min = 120,
-							       .width_max = 120,
-							       .height_min = 84,
-							       .height_max = 84,
+							       .width_min = HTPA_WIDTH_120X84,
+							       .width_max = HTPA_WIDTH_120X84,
+							       .height_min = HTPA_HEIGHT_120X84,
+							       .height_max = HTPA_HEIGHT_120X84,
 							       .width_step = 0,
 							       .height_step = 0,
 						       },
 						       {0}};
 
 #define HM_HTPA_INIT(inst)                                                                         \
-	static int16_t __aligned(4) hm_htpa_pixels_##inst[HTPA_FRAME_QUEUE_SIZE][120 * 84];        \
-	static uint8_t __aligned(4) hm_htpa_raw_top_##inst[6][1682];                               \
+	static int16_t __aligned(4) hm_htpa_pixels_##inst[HTPA_FRAME_QUEUE_SIZE]                   \
+				   [HTPA_WIDTH_120X84 * HTPA_HEIGHT_120X84];                     \
+	static uint8_t __aligned(4)                                                               \
+		hm_htpa_raw_top_##inst[HTPA_BLOCK_COUNT_120X84][HTPA_BLOCK_LENGTH_120X84];         \
 	static uint8_t                                                                             \
-		__aligned(4) hm_htpa_raw_bottom_##inst[6][1682];                                  \
-	static uint8_t __aligned(4) hm_htpa_el_top_##inst[1682];                                   \
-	static uint8_t __aligned(4) hm_htpa_el_bottom_##inst[1682];                                \
+		__aligned(4) hm_htpa_raw_bottom_##inst[HTPA_BLOCK_COUNT_120X84]                    \
+						 [HTPA_BLOCK_LENGTH_120X84];                     \
+	static uint8_t                                                                             \
+		__aligned(4) hm_htpa_el_top_##inst[HTPA_BLOCK_LENGTH_120X84]; \
+	static uint8_t                                                                             \
+		__aligned(4) hm_htpa_el_bottom_##inst[HTPA_BLOCK_LENGTH_120X84]; \
 	static const struct hm_htpa_config hm_htpa_cfg_##inst = {                                  \
 		.hm_htpa_caps = hm_htpa_caps_120x84,                                               \
 		.calibration_flash = DEVICE_DT_GET(DT_INST_PHANDLE(inst, calibration_flash)),      \
--- drivers/video/video_hm_htpa/htpa-160x120.c
+++ drivers/video/video_hm_htpa/htpa-160x120.c
@@ -13,17 +13,27 @@ LOG_MODULE_REGISTER(htpa_160x120, CONFIG_VIDEO_LOG_LEVEL);
 
 #define HTPA_DEFAULT_ACQUISITION_TIME_USEC 1000
 
+enum {
+	HTPA_WIDTH_160X120 = 160,
+	HTPA_HEIGHT_160X120 = 120,
+	HTPA_BLOCK_COUNT_160X120 = 12,
+	HTPA_PIXELS_PER_BLOCK_160X120 = 800,
+	HTPA_BLOCK_LENGTH_160X120 = 1606,
+	HTPA_DATA_OFFSET_160X120 = 6,
+};
+
 #if defined(CONFIG_VIDEO_HM_HTPA_AUTOSCALE_HISTOGRAM)
-BUILD_ASSERT((160 * 120) <= UINT16_MAX, "Histogram counter type is too small");
+BUILD_ASSERT((HTPA_WIDTH_160X120 * HTPA_HEIGHT_160X120) <= UINT16_MAX,
+	     "Histogram counter type is too small");
 #endif
 
 static const struct hm_htpa_sensor_config hm_htpa_sensor_cfg_160x120 = {
-	.width = 160,
-	.height = 120,
-	.block_count = 12,
-	.pixels_per_block = 800,
-	.block_length = 1606,
-	.data_offset = 6,
+	.width = HTPA_WIDTH_160X120,
+	.height = HTPA_HEIGHT_160X120,
+	.block_count = HTPA_BLOCK_COUNT_160X120,
+	.pixels_per_block = HTPA_PIXELS_PER_BLOCK_160X120,
+	.block_length = HTPA_BLOCK_LENGTH_160X120,
+	.data_offset = HTPA_DATA_OFFSET_160X120,
 	.e_id = {0x0074, 0x0075, 0x0076, 0x0077},
 	.e_mbit_calib = 0x001a,
 	.e_bias_calib = 0x001b,
@@ -33,22 +43,27 @@ static const struct hm_htpa_sensor_config hm_htpa_sensor_cfg_160x120 = {
 
 static const struct video_format_cap hm_htpa_caps_160x120[] = {{
 							       .pixelformat = VIDEO_PIX_FMT_Y16,
-							       .width_min = 160,
-							       .width_max = 160,
-							       .height_min = 120,
-							       .height_max = 120,
+							       .width_min = HTPA_WIDTH_160X120,
+							       .width_max = HTPA_WIDTH_160X120,
+							       .height_min = HTPA_HEIGHT_160X120,
+							       .height_max = HTPA_HEIGHT_160X120,
 							       .width_step = 0,
 							       .height_step = 0,
 						       },
 						       {0}};
 
 #define HM_HTPA_INIT(inst)                                                                         \
-	static int16_t __aligned(4) hm_htpa_pixels_##inst[HTPA_FRAME_QUEUE_SIZE][160 * 120];       \
-	static uint8_t __aligned(4) hm_htpa_raw_top_##inst[12][1606];                              \
+	static int16_t __aligned(4) hm_htpa_pixels_##inst[HTPA_FRAME_QUEUE_SIZE]                   \
+				 [HTPA_WIDTH_160X120 * HTPA_HEIGHT_160X120];                     \
+	static uint8_t __aligned(4)                                                               \
+		hm_htpa_raw_top_##inst[HTPA_BLOCK_COUNT_160X120][HTPA_BLOCK_LENGTH_160X120];       \
 	static uint8_t                                                                             \
-		__aligned(4) hm_htpa_raw_bottom_##inst[12][1606];                                 \
-	static uint8_t __aligned(4) hm_htpa_el_top_##inst[1606];                                   \
-	static uint8_t __aligned(4) hm_htpa_el_bottom_##inst[1606];                                \
+		__aligned(4) hm_htpa_raw_bottom_##inst[HTPA_BLOCK_COUNT_160X120]                   \
+						  [HTPA_BLOCK_LENGTH_160X120];                    \
+	static uint8_t                                                                             \
+		__aligned(4) hm_htpa_el_top_##inst[HTPA_BLOCK_LENGTH_160X120]; \
+	static uint8_t                                                                             \
+		__aligned(4) hm_htpa_el_bottom_##inst[HTPA_BLOCK_LENGTH_160X120]; \
 	static const struct hm_htpa_config hm_htpa_cfg_##inst = {                                  \
 		.hm_htpa_caps = hm_htpa_caps_160x120,                                              \
 		.calibration_flash = DEVICE_DT_GET(DT_INST_PHANDLE(inst, calibration_flash)),      \
